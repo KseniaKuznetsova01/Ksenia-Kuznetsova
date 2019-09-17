3d240b6706c81c999a15586cf9dd2fad887784f9
KseniaKuznetsova01 <ksenks.one@gmail.com>
2019-09-17 11:57:20 +0700

    Initial project

---
+++ Case1.py
@@ -0,0 +1,29 @@
+# Case1
+# Developers: Kuznetsova K. (50%), Panukova E. (50%)
+import turtle
+
+Def parallelogram (x, y, color)
+     # TODO:(Ksysha)
+     pass
+Def square (x, y, color)
+    # TODO:(Ksysha)
+    pass
+Def triangle_0(x, y, red):
+    # TODO:(Kate)
+    pass
+Def triangle_1
+    #TODO:(Kate)
+    pass
+Def triangle_2(x, y, color)
+    # TODO:(Ksysha)
+    pass
+Def triangle_3
+    # TODO:(Kate)
+    pass
+Def triangle_4(x, y, color)
+    # TODO:(Kate)
+    pass
+Def main():
+
+
+
