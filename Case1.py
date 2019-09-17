4b5cb6f0e6396c0d50be4b5e1e079ace1d8dcfdb
KseniaKuznetsova01 <ksenks.one@gmail.com>
2019-09-17 12:11:58 +0700

    Initial project

--- Case1.py
+++ Case1.py
@@ -11,13 +11,13 @@ def square (x, y, color)
 def triangle_0(x, y, red):
     # TODO:(Kate)
     pass
-def triangle_1
+def triangle_1(x, y, color)
     #TODO:(Kate)
     pass
 def triangle_2(x, y, color)
     # TODO:(Ksysha)
     pass
-def triangle_3
+def triangle_3(x, y, color)
     # TODO:(Kate)
     pass
 def triangle_4(x, y, color)
